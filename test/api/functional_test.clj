1c74d6ccbbed192bd7cba66cb4bfe8f5b4d8bfd7
dannicolici <dannicolici@gmail.com>
2018-08-21 16:06:55 +0300

    refactor tests

--- test/api/functional_test.clj
+++ test/api/functional_test.clj
@@ -22,19 +22,19 @@
 (use-fixtures :once wrap-all-tests)
 (use-fixtures :each data-fixture)
 
-(defn create-and-assert-user [id]
+(defn create-user-and-assert-response [id]
   (let [response (non-secure-app (mock/request :post "/api/user" {"id" id, "fname" "john", "lname" "doe", "pwd" (encrypt "pwd")}))]
     (is (= (:status response) 201))
     (is (= (:body response) "created"))))
 
-(defn create-and-assert-news [user-id news]
+(defn create-news-and-assert-response [user-id news]
   (let [response (non-secure-app (-> (mock/request :post (str "/api/news/" user-id) news)))]
     (is (= (:status response) 201))
     (is (= (:body response) "created"))))
 
 (deftest test-user
   (testing "put user"
-    (create-and-assert-user "nickname"))
+    (create-user-and-assert-response "nickname"))
 
   (testing "get user"
     (let [user (p/get-user-by-id "nickname")]
@@ -54,12 +54,12 @@
       (is (= (:status response) 409))))
 
   (testing "post news for existing user"
-    (create-and-assert-user "zzz")
-    (create-and-assert-news "zzz" {"text" "some news text"})
+    (create-user-and-assert-response "zzz")
+    (create-news-and-assert-response "zzz" {"text" "some news text"})
     (let [response (non-secure-app (mock/request :get "/api/news/zzz"))]
       (is (= (:status response) 200))
       (is (str/includes? (:body response) "{\"id\":\"1\",\"user-id\":\"zzz\",\"text\":\"some news text\"")))
-    (create-and-assert-news "zzz" {"text" "news2"})
+    (create-news-and-assert-response "zzz" {"text" "news2"})
     (let [response (non-secure-app (mock/request :get "/api/news/zzz"))]
       (is (= (:status response) 200))
       (is (let [resp (:body response)]
@@ -67,10 +67,11 @@
                  (str/includes? resp "{\"id\":\"2\",\"user-id\":\"zzz\",\"text\":\"news2\""))))))
 
   (testing "user in session can post news"
+    ;we need to bypass ring's wrap-session to mock the session
     (let [request (mock/request :post "/api/news")
           session-request (assoc-in request [:session :cemerick.friend/identity :current] "zzz")
           params-request (assoc-in session-request [:params] {"text" "user in session"})
-          response (api-routes params-request)] ;we need to bypass ring's wrap-session to mock the session
+          response (api-routes params-request)]
       (is (= (:status response) 201))
       (is (= (:body response) "created")))
     (let [response (non-secure-app (mock/request :get "/api/news/zzz"))]
